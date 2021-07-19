85e0cee38e145f1c829c4063490842ca19add3eb
Dario Cazzani <dariocazzani@gmail.com>
2021-07-19 16:39:20 +0000

    Use PyVAD

--- void/utils.py
+++ void/utils.py
@@ -1,19 +1,25 @@
-from inaSpeechSegmenter import Segmenter as Seg
-import soundfile as sf
 import numpy as np
-import os
+from pyvad import vad
 
 class Segmenter(object):
-    def __init__(self):
-        self.seg = Seg()
-        
-    def __call__(self, audio:np.array, sr:int=16000) -> list:
-        temp = "/tmp/asd.wav"
-        sf.write(temp, audio[:, None], sr)
-        segments = self.seg(temp)
-        os.remove(temp)
-        segments = [(int(segment[1]*sr), int(segment[2]*sr)) for segment in segments if segment[0] in ["male", "female"]]
+    def __call__(self, audio:np.array, sr:int=16000, hop_length:int=30, vad_mod:int=3) -> list:
+        vact = vad(audio, sr, fs_vad=sr, hop_length=hop_length, vad_mode=vad_mod)
+        segments = list()
+        previous = 0
+        for idx, pred in enumerate(vact):
+            if previous == 0 and pred == 1:
+                start = idx
+                previous = 1
+            if previous == 1 and pred == 0:
+                end = idx
+                previous = 0
+                segments.append((start, end))
+        if previous == 1 and pred == 1:
+            end = len(vact)
+            segments.append((start, end))
+
         return segments
+    
 
 def rttm2simple(rttm:list) -> list:
     """
